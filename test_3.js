12611de8e2ba44bc82a04968eae1f7df8b67cffa
shiryaev585 <alex_sh_v@mail.ru>
2021-06-17 09:54:59 +0300

    fixed test-3

--- test_3.js
+++ test_3.js
@@ -1,9 +1,12 @@
 for(let i = 1; i <= 100; i++) {
-    if(i % 3 === 0) {
+    if(i % 3 === 0 && i % 5 != 0) {
+        i++
         console.log("A/B")
-    } else if (i % 5 === 0){
+    } else if (i % 5 === 0 && i % 3 != 0){
+        i++
         console.log("Testing")
     } else if (i % 3 === 0 && i % 5 === 0) {
+        i++
         console.log("A/B Testing")
     }
     console.log(i)
